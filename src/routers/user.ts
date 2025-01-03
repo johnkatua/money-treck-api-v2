67f11cb23cd3bf156da82be05634d04dbc23c4c5
johnkatua <johnkatua99@gmail.com>
2025-01-03 10:56:24 +0300

    add user routers

---
+++ src/routers/user.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { IUser } from "../models/User";
+import { loginUser, registerUser } from "../controllers/user";
+import auth, { CustomRequest } from "../middleware/auth";
+
+const router = Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userData: Partial<IUser> = {
+    name, email, password
+  }
+
+  const registeredUser = await registerUser(userData)
+  if (registeredUser?.error) {
+    return res.status(400).json({
+      msg: registeredUser?.error
+    })
+  }
+
+  return res.status(201).json(registeredUser)
+})
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userData: Partial<IUser> = {
+    email, password
+  }
+
+  const loggedInUser = await loginUser(userData)
+
+  if (loggedInUser?.error) {
+    return res.status(400).json({
+      msg: loggedInUser?.error
+    })
+  }
+
+  return res.status(200).json(loggedInUser)
+})
