6702ca319801b4e1acc6898b7325177c76f88371
johnkatua <johnkatua99@gmail.com>
2025-01-05 17:48:59 +0300

    add a post route to create a new revenue

--- src/controllers/revenue.ts
+++ src/controllers/revenue.ts
@@ -1,15 +1,22 @@
 import { IRevenue } from "../interface/revenue";
 import RevenueModel from "../models/revenue";
 
-export const createRevenue = async (revenueData: Partial<IRevenue>) => {
+export const createRevenue = async (req, res) => {
   try {
-    const data = await RevenueModel.create(revenueData)
-    return {
-      data, success: true
-    }
+    const { name, amount, period } = req;
+    const user_id = req.user._id
+    const data = await RevenueModel.create({
+      name, amount, period, user_id
+    })
+    
+    res.status(201).json({
+      msg: "Revenue created successfully",
+      data
+    })
   } catch (error) {
-    return {
-      data: null, success: error
-    }
+    res.status(500).json({
+      msg: "Failed to create revenue",
+      error: error.message
+    })
   }
 }
---
+++ src/routers/revenue.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import auth from "../middleware/auth";
+import { createRevenue } from "../controllers/revenue";
+
+const router = Router();
+
+// Post /api/revenues - Create a new revenue
+router.post("/", auth, createRevenue)
