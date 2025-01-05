c0002db71cbf2d1310514a26cd1f72c42922cdc7
johnkatua <johnkatua99@gmail.com>
2025-01-05 10:12:01 +0300

    add budget schema

--- src/models/budget.ts
+++ src/models/budget.ts
@@ -1,12 +1,12 @@
 import { model, Schema } from "mongoose";
-import { IRevenue } from "../interface/revenue";
+import { IBudget } from "../interface/budget";
 import { PeriodInterface } from "../interface/period";
 
-const revenueSchema = new Schema<IRevenue>({
+const budgetSchema = new Schema<IBudget>({
   name: { type: String, required: true, unique: true, index: true },
   amount: { type: Number, required: true },
   period: { type: String, required: true, enum: Object.values(PeriodInterface) },
   user_id: { type: String, required: true }
 }, { timestamps: true })
 
-export default model<IRevenue>('Revenue', revenueSchema)
+export default model<IBudget>('Budget', budgetSchema);
