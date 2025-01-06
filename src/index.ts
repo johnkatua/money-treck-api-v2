30d4d66311d373773ef26a7985b9f4a74ad453e5
johnkatua <johnkatua99@gmail.com>
2025-01-06 13:48:03 +0300

    add expenditure router to the entry point

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import userRouter from "./routers/user";
 import revenueRouter from "./routers/revenue";
 import budgetRouter from "./routers/budget";
+import expenditureRouter from "./routers/expenditure";
 import "./db";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/api/users", userRouter);
 app.use("/api/revenues", revenueRouter);
 app.use("/api/budgets", budgetRouter);
+app.use("/api/expenditures", expenditureRouter);
 
 app.get("/healthcheck", (req: Request, res: Response) => {
   res.send('API healthcheck!')
