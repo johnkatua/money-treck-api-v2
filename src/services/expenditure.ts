bd9145c2290ed68b7d0c1df6b9c16830d322392d
johnkatua <johnkatua99@gmail.com>
2025-01-06 13:42:16 +0300

    add a service to create new expenditure

---
+++ src/services/expenditure.ts
@@ -0,0 +1,28 @@
+import { Response } from "express";
+import { CustomRequest } from "../middleware/auth";
+import { IExpenditure } from "../interface/expenditure";
+import { createExpenditure } from "../controllers/expenditure";
+
+export const create = async (req: CustomRequest, res: Response) => {
+  try {
+    const { name, amount, budget_id } = req.body;
+    const user_id = req.user?._id; // user_id retrieved from auth token
+
+    const expenditureData: Partial<IExpenditure> = {
+      name, amount, budget_id, user_id
+    }
+
+    const data = await createExpenditure(expenditureData);
+
+    res.status(201).json({
+      msg: "Expenditure created successfully",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to create expenditure",
+      error: errorMessage
+    })
+  }
+}
