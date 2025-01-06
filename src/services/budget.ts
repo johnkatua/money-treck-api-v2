abd9eae204bacce197ccbf54e34c40f15c9e2318
johnkatua <johnkatua99@gmail.com>
2025-01-06 09:40:27 +0300

    add a service to get all budgets

--- src/services/budget.ts
+++ src/services/budget.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { CustomRequest } from "../middleware/auth";
 import { IBudget } from "../interface/budget";
-import { createBudget } from "../controllers/budget";
+import { createBudget, getBudgets } from "../controllers/budget";
 
 export const create = async (req: CustomRequest, res: Response) => {
   try {
@@ -22,6 +22,23 @@ export const create = async (req: CustomRequest, res: Response) => {
     const errorMessage = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({
       msg: "Failed to create budget",
+      error: errorMessage
+    })
+  }
+}
+
+export const getAll = async (req: CustomRequest, res: Response) => {
+  try {
+    const data = await getBudgets();
+
+    res.status(200).json({
+      msg: "Budgets fetched successfully!",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to fetch budgets",
       error: errorMessage
     })
   }
