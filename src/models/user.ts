a8e92a1b01b530e9889fb6bd000ffc7ffc94d2da
johnkatua <johnkatua99@gmail.com>
2025-01-06 08:42:13 +0300

    fix the values stored in the user token

--- src/models/user.ts
+++ src/models/user.ts
@@ -41,7 +41,7 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this
-  const token = jwt.sign({ _id: user.name.toString() }, process.env.JWT_KEY as string)
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY as string)
   user.tokens = user.tokens.concat({ token })
   await user.save()
   return token
