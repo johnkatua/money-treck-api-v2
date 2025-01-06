24b7e8fc605244e3cece864294ffaa70be6b8101
johnkatua <johnkatua99@gmail.com>
2025-01-06 09:04:58 +0300

    add a controller to create new budget

---
+++ src/controllers/budget.ts
@@ -0,0 +1,11 @@
+import { IBudget } from "../interface/budget";
+import BudgetModel from "../models/budget";
+
+export const createBudget = async (budgetData: Partial<IBudget>) => {
+  try {
+    const data = await BudgetModel.create(budgetData)
+    return { data, success: true }
+  } catch (error) {
+    return { data: null, success: false, error }
+  }
+}
