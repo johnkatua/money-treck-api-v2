99a8dee329aefec387ecfa63c6da1e255c8adedd
johnkatua <johnkatua99@gmail.com>
2025-01-06 09:15:48 +0300

    add a service to create budget

---
+++ src/services/budget.ts
@@ -0,0 +1,28 @@
+import { Response } from "express";
+import { CustomRequest } from "../middleware/auth";
+import { IBudget } from "../interface/budget";
+import { createBudget } from "../controllers/budget";
+
+export const create = async (req: CustomRequest, res: Response) => {
+  try {
+    const { name, amount, period } = req.body;
+    const user_id = req.user?._id;
+
+    const budgetData: Partial<IBudget> = {
+      name, amount, period, user_id
+    }
+
+    const data = await createBudget(budgetData);
+
+    res.status(201).json({
+      msg: "Budget created successfully",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to create budget",
+      error: errorMessage
+    })
+  }
+}
