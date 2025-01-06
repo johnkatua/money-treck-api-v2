f8de787f9d74f218b0d056025ad9f1ea1454100b
johnkatua <johnkatua99@gmail.com>
2025-01-06 14:36:33 +0300

    add a route to get budget by id

--- src/routers/budget.ts
+++ src/routers/budget.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create, getAll } from "../services/budget";
+import { create, getAll, getById } from "../services/budget";
 import auth from "../middleware/auth";
 
 const router = Router();
 
 router.post("/", auth, create);
 router.get("/", auth, getAll);
+router.get("/", auth, getById)
 
 export default router;
