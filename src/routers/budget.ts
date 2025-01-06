374ab5acac2f17f1f7a8f977e14fd5181cebdb94
johnkatua <johnkatua99@gmail.com>
2025-01-06 09:26:11 +0300

    add a create budget route

--- src/routers/budget.ts
+++ src/routers/budget.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
+import { create } from "../services/budget";
 import auth from "../middleware/auth";
-import { create, getAll } from "../services/revenue";
 
 const router = Router();
 
 router.post("/", auth, create)
-router.get("/", auth, getAll)
-
 
 export default router;
