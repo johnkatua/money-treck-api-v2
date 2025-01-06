df153d6a8bd0e2ade381be79854f55594e1f4d0b
johnkatua <johnkatua99@gmail.com>
2025-01-06 14:03:06 +0300

    add a service to get all expenditures

--- src/services/expenditure.ts
+++ src/services/expenditure.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { CustomRequest } from "../middleware/auth";
 import { IExpenditure } from "../interface/expenditure";
-import { createExpenditure } from "../controllers/expenditure";
+import { createExpenditure, getExpenditures } from "../controllers/expenditure";
 
 export const create = async (req: CustomRequest, res: Response) => {
   try {
@@ -22,6 +22,23 @@ export const create = async (req: CustomRequest, res: Response) => {
     const errorMessage = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({
       msg: "Failed to create expenditure",
+      error: errorMessage
+    })
+  }
+}
+
+export const getAll = async (req: CustomRequest, res: Response) => {
+  try {
+    const data = await getExpenditures();
+
+    res.status(200).json({
+      msg: "Expenditures fetched successfully!",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to fetch expenditures",
       error: errorMessage
     })
   }
