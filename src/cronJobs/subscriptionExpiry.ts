7f03087c7a801709836617844ece5874913d426d
johnkatua <johnkatua99@gmail.com>
2025-01-07 10:25:33 +0300

    add a subscription expiry cron job

--- src/cronJobs/subscriptionExpiry.ts
+++ src/cronJobs/subscriptionExpiry.ts
@@ -1,1 +1,12 @@
 import cron from "node-cron";
+import SubscriptionModel from "../models/subscription";
+
+cron.schedule("0 0 * * *", async () => { 
+  const now = new Date();
+  await SubscriptionModel.updateMany(
+    { endDate: { $lt: now }, status: "Active" },
+    { status: "Expired" }
+  )
+
+  console.log("Expired subscriptions updated!")
+});
