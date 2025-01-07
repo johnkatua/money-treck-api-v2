a4d5543ac57f265b8be725ec01ca87ded534bbcf
johnkatua <johnkatua99@gmail.com>
2025-01-07 16:33:41 +0300

    update generate mpesa token functionality

--- src/utils/generateMpesaToken.ts
+++ src/utils/generateMpesaToken.ts
@@ -1,7 +1,19 @@
+import axios from "axios";
+
 export const generateMpesaToken = async () => {
   try {
-    const tokenResponse = await axios.post()
+    const tokenResponse = await axios.post(
+      'url',
+      {
+        auth: {
+          username: '',
+          password: ''
+        }
+      }
+    )
+
+    return tokenResponse.data.access_token
   } catch (error) {
-    
+    console.error(error)
   }
 }
