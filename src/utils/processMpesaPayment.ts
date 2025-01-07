a772083fa4ca28a31448e32501281f3d3f26e976
johnkatua <johnkatua99@gmail.com>
2025-01-07 16:42:21 +0300

    update payment status when payment process fails

--- src/utils/processMpesaPayment.ts
+++ src/utils/processMpesaPayment.ts
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { generateMpesaToken } from "./generateMpesaToken"
+import PaymentModel from "../models/payment";
+
 export const processMpesaPayment = async (amount: number, paymentId: string) => {
   try {
-    
+    const token = await generateMpesaToken();
+
+    // Initiate Mpesa payment process
+    const response = await axios.post(
+      '',
+      {
+
+      },
+      {
+        headers: {
+        Authorization: `Bearer ${token}`
+      }}
+    )
+
+    return response.data
   } catch (error) {
-    
+    console.error(error);
+    await PaymentModel.findByIdAndUpdate(paymentId, { paymentStatus: 'Failed' });
+    throw new Error('Mpesa payment failed');
   }
 }
