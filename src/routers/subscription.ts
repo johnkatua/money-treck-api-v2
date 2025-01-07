fcdf381806ac827421e85d673a2818c04853a59e
johnkatua <johnkatua99@gmail.com>
2025-01-07 09:11:54 +0300

    add user subscriptions route

--- src/routers/subscription.ts
+++ src/routers/subscription.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { create } from "../services/subscription";
+import { create, getUserSubscriptionsService } from "../services/subscription";
 import auth from "../middleware/auth";
 
 const router: Router = Router();
 
 router.post("/subscribe", auth, create);
+router.get("/user/:user_id", auth, getUserSubscriptionsService)
 
 export default router;
