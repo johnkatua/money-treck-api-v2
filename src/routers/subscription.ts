2fa7fca69c9384c5b5ea0e1c0c49606481978378
johnkatua <johnkatua99@gmail.com>
2025-01-07 09:37:54 +0300

    add a cancel subscription route

--- src/routers/subscription.ts
+++ src/routers/subscription.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create, getUserSubscriptionsService } from "../services/subscription";
+import { cancelSubscriptionService, create, getUserSubscriptionsService } from "../services/subscription";
 import auth from "../middleware/auth";
 
 const router: Router = Router();
 
 router.post("/subscribe", auth, create);
-router.get("/user/:user_id", auth, getUserSubscriptionsService)
+router.get("/user/:user_id", auth, getUserSubscriptionsService);
+router.patch("/cancel/:id", auth, cancelSubscriptionService)
 
 export default router;
