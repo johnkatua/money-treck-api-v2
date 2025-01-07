eb7575249151b1304c1566fdedccf237c17b4acf
johnkatua <johnkatua99@gmail.com>
2025-01-07 08:58:06 +0300

    add a controller to get all user subscriptions

--- src/controllers/subscription.ts
+++ src/controllers/subscription.ts
@@ -1,9 +1,18 @@
 import { SubscriptionRequestBody } from "../interface/subscription";
-import subscription from "../models/subscription";
+import SubscriptionModel from "../models/subscription";
 
 export const createSubscription = async (subscriptionData: Partial<SubscriptionRequestBody>) => {
   try {
-    const data = await subscription.create(subscriptionData);
+    const data = await SubscriptionModel.create(subscriptionData);
+    return { data, success: true }
+  } catch (error) {
+    return { data: null, success: false, error }
+  }
+}
+
+export const getUserSubscriptions = async (user_id: string) => {
+  try {
+    const data = await SubscriptionModel.find({ user_id });
     return { data, success: true }
   } catch (error) {
     return { data: null, success: false, error }
