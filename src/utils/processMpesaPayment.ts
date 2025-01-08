a5be93275cd91381dd8820638c86f8017c505df9
johnkatua <johnkatua99@gmail.com>
2025-01-08 10:19:44 +0300

    add a body for stkpush

--- src/utils/processMpesaPayment.ts
+++ src/utils/processMpesaPayment.ts
@@ -10,9 +10,19 @@ export const processMpesaPayment = async (amount: number, paymentId: string) => 
 
     // Initiate Mpesa payment process
     const response = await axios.post(
-      '',
+      'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
       {
-        
+        BusinessShortCode: MPESA_SHORTCODE,
+        Password: MPESA_PASSWORD,
+        Timestamp: new Date().toISOString(),
+        TransactionType: 'CustomerPaybillOnline',
+        Amount: amount,
+        PartyA: 'USER_PHONE_NUMBER',
+        PartyB: MPESA_SHORTCODE,
+        PhoneNumber: 'USER_PHONE_NUMBER',
+        CallBackURL: `${BASE_URL}/mpesa/callback`,
+        AccountReference: paymentId,
+        TransactionDesc: 'Payment for Subscription'
       },
       {
         headers: {
