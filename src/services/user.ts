7c1bf1878a8b68eb804b438c6ada0c7b70050961
johnkatua <johnkatua99@gmail.com>
2025-02-03 18:35:35 +0300

    update get-user service

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,26 @@
+import { Response } from "express";
+import { getUserById } from "../controllers/user";
 import { CustomRequest } from "../middleware/auth";
+
 
 export const getUser = async (req: CustomRequest, res: Response) => {
   try {
+    const id = req.user?._id;
+  
+
+    const data = await getUserById(id!);
+
+    res.status(200).json({
+      msg: "User fetched successfully",
+      data
+    })
+
     
   } catch (error) {
-    
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to fetch user",
+      error: errorMessage
+    })
   }
 }
