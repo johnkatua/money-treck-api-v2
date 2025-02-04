ae850c14b06480a6e7bd40223c111dbd6d50baa0
johnkatua <johnkatua99@gmail.com>
2025-02-04 18:34:57 +0300

    update subscription model

--- src/models/subscription.ts
+++ src/models/subscription.ts
@@ -3,7 +3,7 @@ import { ISubscription } from "../interface/subscription";
 
 const subscriptionSchema = new Schema<ISubscription>({
   user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  planName: { type: String, enum: ["Free", "Basic", "Premium"], required: true },
+  planName: { type: String, enum: ["Monthly", "Yearly"], required: true },
   price: { type: Number, required: true },
   startDate: { type: Date, default: Date.now },
   endDate: { type: Date, required: true },
