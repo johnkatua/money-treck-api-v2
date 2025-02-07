611268f8019f44454db42c5d9230d356dd35114c
johnkatua <johnkatua99@gmail.com>
2025-02-07 19:48:36 +0300

    add a service to update budget

--- src/services/budget.ts
+++ src/services/budget.ts
@@ -1,8 +1,7 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { CustomRequest } from "../middleware/auth";
 import { IBudget } from "../interface/budget";
-import { createBudget, getBudgetById, getBudgets } from "../controllers/budget";
-import { IExpenditure } from "../interface/expenditure";
+import { createBudget, getBudgetById, getBudgets, updateBudget } from "../controllers/budget";
 
 export const create = async (req: CustomRequest, res: Response) => {
   try {
@@ -65,6 +64,30 @@ export const getById = async (req: CustomRequest, res: Response): Promise<any> =
     const errorMessage = error instanceof Error ? error.message : "Unknown error";
     res.status(500).json({
       msg: `Failed to fetch budget with an id of ${id}`,
+      error: errorMessage
+    })
+  }
+}
+
+export const updateBudgetService = async (req: CustomRequest, res: Response) => {
+  try {
+    const { name, amount, period, _id } = req.body;
+    const user_id = req.user?._id; // user_id retrieved from auth token
+
+    const budgetData: Partial<IBudget> = {
+      name, amount, period, user_id, _id
+    }
+
+    const data = await updateBudget(budgetData);
+
+    res.status(200).json({
+      msg: "Budget updated successfully",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      msg: "Failed to update budget",
       error: errorMessage
     })
   }
