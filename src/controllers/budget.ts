2581ac3ddf5329f25061ec4a5109fdef4965d9d6
johnkatua <johnkatua99@gmail.com>
2025-02-07 19:23:11 +0300

    add controllers to update and delete budgets

--- src/controllers/budget.ts
+++ src/controllers/budget.ts
@@ -1,5 +1,6 @@
 import { IBudget } from "../interface/budget";
 import BudgetModel from "../models/budget";
+import ExpenditureModel from "../models/expenditure";
 
 export const createBudget = async (budgetData: Partial<IBudget>) => {
   try {
@@ -22,6 +23,28 @@ export const getBudgets = async () => {
 export const getBudgetById = async (id: string) => {
   try {
     const data = await BudgetModel.findById(id);
+    return { data, success: true }
+  } catch (error) {
+    return { data: null, success: false, error }
+  }
+}
+
+export const updateBudget = async (budgetData: Partial<IBudget>) => { 
+  try {
+    const { _id } = budgetData;
+    const data = await BudgetModel.findByIdAndUpdate
+      (_id, budgetData, { new: true })
+    return { data, success: true }
+  } catch (error) {
+    return { data: null, success: false, error }
+  }
+}
+
+export const deleteBudget = async (id: string) => {
+  try {
+    const data = await BudgetModel.findByIdAndDelete(id)
+    // delete entries that reference this budget in expenditures
+    await ExpenditureModel.deleteMany({ budget_id: id })
     return { data, success: true }
   } catch (error) {
     return { data: null, success: false, error }
