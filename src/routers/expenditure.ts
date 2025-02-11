7ffb352b7dbfcbaf4b8031559c7031da681574e4
johnkatua <johnkatua99@gmail.com>
2025-02-11 16:48:48 +0300

    add delete service for expenditure management

--- src/routers/expenditure.ts
+++ src/routers/expenditure.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import auth from "../middleware/auth";
-import { create, getAll, updateExpenditureService } from "../services/expenditure";
+import { create, deleteExpenditureService, getAll, updateExpenditureService } from "../services/expenditure";
 
 const router = Router();
 
 router.post("/", auth, create);
 router.get("/", auth, getAll);
 router.put("/", auth, updateExpenditureService);
+router.delete("/:id", auth, deleteExpenditureService);
 
 export default router;
