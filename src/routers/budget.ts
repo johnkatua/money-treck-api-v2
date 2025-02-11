b92288f1aa6b106b16b600a547433ba41712df06
johnkatua <johnkatua99@gmail.com>
2025-02-11 15:49:30 +0300

    add update and delete services for budget management

--- src/routers/budget.ts
+++ src/routers/budget.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { create, getAll, getById } from "../services/budget";
 import auth from "../middleware/auth";
+import { create, deleteBudgetService, getAll, getById, updateBudgetService } from "../services/budget";
 
 const router = Router();
 
 router.post("/", auth, create);
 router.get("/", auth, getAll);
-router.get("/", auth, getById)
+router.get("/", auth, getById);
+router.put("/", auth, updateBudgetService);
+router.delete("/:id", auth, deleteBudgetService)
 
 export default router;
