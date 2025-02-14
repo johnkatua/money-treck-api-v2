18e8cc38944be9bc43f72d1334667abc76b7bf09
johnkatua <johnkatua99@gmail.com>
2025-02-14 15:58:06 +0300

    feat: implement function to generate PayPal OAuth2 token

--- src/utils/generatePaypalToken.ts
+++ src/utils/generatePaypalToken.ts
@@ -3,19 +3,17 @@ import axios from "axios";
 export const generatePaypalToken = async () => {
   const { PAYPAL_BASE_URL, PAYPAL_CLIENT_ID, PAYPAL_SECRET_KEY } = process.env;
   try {
+    const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET_KEY}`).toString("base64")
     const tokenResponse = await axios.post(
       `${PAYPAL_BASE_URL}/v1/oauth2/token`,
-      {
-        data: "grant_type=client_credentials",
-      },
+      "grant_type=client_credentials",
       {
         headers: {
-          username: PAYPAL_CLIENT_ID,
-          password: PAYPAL_SECRET_KEY,
+          Authorization: `Basic ${auth}`
         },
       }
     );
-    console.log(tokenResponse);
+    return tokenResponse.data.access_token;
   } catch (error) {
     console.error(error);
   }
