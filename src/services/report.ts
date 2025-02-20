80c185075ab4033147185cfabd583f5301523f12
johnkatua <johnkatua99@gmail.com>
2025-02-20 21:24:06 +0300

    feat: add endpoint to fetch financial overview

--- src/services/report.ts
+++ src/services/report.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getBudgetVsExpense, getRevenueTrends } from "../controllers/report"
+import { getBudgetVsExpense, getFinancialOverview, getRevenueTrends } from "../controllers/report"
 
 export const revenueTrends = async (req: Request, res: Response) => {
   try {
@@ -32,6 +32,24 @@ export const budgetVsExpenses = async (req: Request, res: Response) => {
 
     res.status(500).json({
       msg: "Failed to fetch budget and expenses analysis",
+      error: errorMessage
+    })
+  }
+}
+
+export const financialOverview = async (req: Request, res: Response) => {
+  try {
+    const data = await getFinancialOverview();
+
+    res.status(200).json({
+      msg: "Financial report fetched successfully",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+
+    res.status(500).json({
+      msg: "Failed to fetch financial report",
       error: errorMessage
     })
   }
