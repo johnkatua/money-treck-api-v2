bb9f436ebf3d6cbf5f54c3e365569b3056f9b5b7
johnkatua <johnkatua99@gmail.com>
2025-02-20 20:59:20 +0300

    feat: add endpoint to fetch budget vs expenses analysis

--- src/services/report.ts
+++ src/services/report.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import { getRevenueTrends } from "../controllers/report"
+import { getBudgetVsExpense, getRevenueTrends } from "../controllers/report"
 
 export const revenueTrends = async (req: Request, res: Response) => {
   try {
     const data = await getRevenueTrends();
 
-    res.status(201).json({
+    res.status(200).json({
       msg: "Revenue trends fetched successfully",
       data
     })
@@ -14,6 +14,24 @@ export const revenueTrends = async (req: Request, res: Response) => {
 
     res.status(500).json({
       msg: "Failed to fetch revenue trends",
+      error: errorMessage
+    })
+  }
+}
+
+export const budgetVsExpenses = async (req: Request, res: Response) => {
+  try {
+    const data = await getBudgetVsExpense();
+
+    res.status(200).json({
+      msg: "Budget and Expenses fetched successfully",
+      data
+    })
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+
+    res.status(500).json({
+      msg: "Failed to fetch budget and expenses analysis",
       error: errorMessage
     })
   }
