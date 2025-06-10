6a79e107a6273fa39dd0cd491a1791498367a350
John Katua <johnkatua99@gmail.com>
2025-06-10 17:07:21 +0300

    fix: clean-up the mpesa callback func

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -1,14 +1,11 @@
-import SubscriptionModel from "../models/subscription"
+import SubscriptionModel from "../models/subscription";
 
 export const mpesaCallback = async (MerchantRequestID: string, ResultCode: number) => {
-  console.log("Here", MerchantRequestID, ResultCode)
   try {
     if (ResultCode !== 0) {
-      console.log("There", ResultCode)
-      const res = await SubscriptionModel.findOneAndUpdate({ merchantRequestID: MerchantRequestID}, {
+      await SubscriptionModel.findOneAndUpdate({ merchantRequestID: MerchantRequestID}, {
         status: 'Cancelled'
-      })
-      console.log({ res })
+      });
       return
     }
     const data = await SubscriptionModel.findOneAndUpdate({ merchantRequestID: MerchantRequestID}, {
