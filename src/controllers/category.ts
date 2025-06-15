0a0797bce3686e7d3d310bdc4777f3ddb702e7ee
John Katua <johnkatua99@gmail.com>
2025-06-15 14:40:52 +0300

    feat: add category controllers

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import { CreateCategoryDto } from "../dto/category.dto"
+import { CreateCategoryDto, UpdateCategoryDto } from "../dto/category.dto"
 import { CategoryService } from "../services/category"
 
 const categoryService = new CategoryService()
@@ -12,7 +12,55 @@ export const createCategory = async (req: Request, res: Response, next: NextFunc
         const category = await categoryService.create(data)
         res.status(201).json({
             data: category,
-            message: 'Category created successfully'
+            msg: 'Category created successfully'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getCategories = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const filter = req.query || {};
+        const categories = await categoryService.findAll(filter)
+        res.json({
+            data: categories
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getCategoryById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const category = await categoryService.findById(id)
+        res.json(category)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const data = UpdateCategoryDto.parse(req.body)
+        const updated = await categoryService.updateById(id, data);
+        res.json({
+            data: updated,
+            msg: 'Category updated successfully'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        await categoryService.deleteById(id)
+        res.json({
+            msg: 'Category deleted successfully'
         })
     } catch (error) {
         next(error)
