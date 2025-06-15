02651148cf7af5f2ab7bc26999438d3fbe3753f0
John Katua <johnkatua99@gmail.com>
2025-06-15 14:48:31 +0300

    feat: add category routes

---
+++ src/routers/category.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createCategory, deleteCategory, getCategories, getCategoryById, updateCategory } from "../controllers/category";
+
+const router = Router()
+
+router.post("/", createCategory);
+router.get("/", getCategories);
+router.get("/:id", getCategoryById);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory)
