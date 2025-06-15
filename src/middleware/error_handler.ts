70728f818eb2d4db775ffdb82f852774c816a9d7
John Katua <johnkatua99@gmail.com>
2025-06-15 14:57:30 +0300

    feat: add an error handler

---
+++ src/middleware/error_handler.ts
@@ -0,0 +1,8 @@
+import { ErrorRequestHandler } from "express"
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).json({
+        msg: err.message || "Internal Server Error"
+    })
+}
