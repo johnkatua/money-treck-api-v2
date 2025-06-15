ced707e96f2d6d0e961cb4093130b336c5f2ee83
John Katua <johnkatua99@gmail.com>
2025-06-15 13:52:55 +0300

    feat: add category dtos to the application

--- src/dto/category.dto.ts
+++ src/dto/category.dto.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
+import { objectIdValidator } from "../utils/validator";
 
 export const CreateCategoryDto = z.object({
     name: z.string().min(2).max(50),
     type: z.enum(["Revenue", "Expense"]),
-    user_id: z.string().uuid(), // Adjust based on your ID system
+    user_id: objectIdValidator(),
     global: z.boolean().optional(),
 })
 
 export const UpdateCategoryDto = z.object({
   name: z.string().min(2).max(50).optional(),
   type: z.enum(["Revenue", "Expense"]).optional(),
-  user_id: z.string().uuid().optional(),
+  user_id: objectIdValidator().optional(),
   global: z.boolean().optional(),
 });
 
