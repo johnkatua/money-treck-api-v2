6851345f839074e0877b61c86b038c80da65125a
John Katua <johnkatua99@gmail.com>
2025-06-20 12:02:57 +0300

    feat: add a transaction interface

---
+++ src/interface/transaction.ts
@@ -0,0 +1,12 @@
+import { Document, ObjectId } from "mongoose";
+
+export interface ITransaction extends Document {
+    category_id: ObjectId
+    user_id: ObjectId,
+    amount: number,
+    type: String,
+    name: String,
+    date: Date,
+    status: String,
+    is_recurring: Boolean
+}
