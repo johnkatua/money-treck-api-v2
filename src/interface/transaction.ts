ed3dcacd0e1f4229ff3036dc170562c0828a89af
John Katua <johnkatua99@gmail.com>
2025-06-22 01:37:50 +0300

    feat: update transaction interface and model

--- src/interface/transaction.ts
+++ src/interface/transaction.ts
@@ -1,12 +1,13 @@
 import { Document, ObjectId } from "mongoose";
+import { TransactionStatus, TransactionType } from "./enums";
 
 export interface ITransaction extends Document {
     category_id: ObjectId
     user_id: ObjectId,
     amount: number,
-    type: String,
+    type: TransactionType,
     name: String,
     date?: Date,
-    status: String,
+    status: TransactionStatus,
     is_recurring: Boolean
 }
--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -1,13 +1,16 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
+import { TransactionStatus, TransactionType } from "../interface/enums";
 import { ITransaction } from "../interface/transaction";
 
 const transactionSchema = new Schema<ITransaction>({
     category_id: { type: Schema.Types.ObjectId, ref: "Category", required: true },
     user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
     amount: { type: Number, required: true },
-    type: { type: String, required: true },
+    type: { type: String, enum: Object.values(TransactionType), required: true },
     name: { type: String, required: true },
     date: { type: String, default: Date.now },
-    status: {},
+    status: {  type: String, enum: Object.values(TransactionStatus) },
     is_recurring: { type: Boolean, default: false }
-})
+}, { timestamps: true })
+
+export default model<ITransaction>('Transaction', transactionSchema)
