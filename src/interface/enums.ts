fb0081fb2e9afbb2f4acfedcdf56d967677f5682
John Katua <johnkatua99@gmail.com>
2025-06-22 01:26:54 +0300

    feat: add transaction type enums

--- src/interface/enums.ts
+++ src/interface/enums.ts
@@ -2,3 +2,15 @@ export enum BillingCycleTypes {
     Monthly = 'monthly',
     Yearly = 'yearly'
 }
+
+export enum RecurrenceInterval {
+  Daily = "Daily",
+  Weekly = "Weekly",
+  Monthly = "Monthly",
+  Yearly = "Yearly"
+}
+
+export enum TransactionType {
+  Revenue = "Revenue",
+  Expense = "Expense"
+}
