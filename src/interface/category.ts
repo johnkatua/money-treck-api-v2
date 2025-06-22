67af3a317e839a6963968cd907e33d6d0ba2fe89
John Katua <johnkatua99@gmail.com>
2025-06-22 03:38:57 +0300

    feat: update category types

--- src/interface/category.ts
+++ src/interface/category.ts
@@ -3,6 +3,6 @@ import { Document } from "mongoose";
 export interface ICategory extends Document {
     name: string,
     user_id: string,
-    type: "Revenue" | "Expense",
+    type: "Revenue" | "Expense" | "Goal",
     global?: Boolean
 }
