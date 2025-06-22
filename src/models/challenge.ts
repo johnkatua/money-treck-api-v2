046f8762e314867aaf3f737432975e1b4065ce83
John Katua <johnkatua99@gmail.com>
2025-06-22 13:12:22 +0300

    add challenge db model

---
+++ src/models/challenge.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+import { IChallenge } from "../interface/challenge";
+import { ChallengeStatus, ChallengeType } from "../interface/enums";
+
+const challengeSchema = new Schema<IChallenge>({
+    title: { type: String, required: true, trim: true, unique: true },
+    description: { type: String, trim: true },
+    type: { type: String, enum: Object.values(ChallengeType), required: true },
+    start_date: { type: Date, default: Date.now },
+    end_date: { type: Date, required: true },
+    participants: { type: [Schema.Types.ObjectId], required: true },
+    rewards: { type: [String], required: true },
+    status: {
+        type: String,
+        enum: Object.values(ChallengeStatus),
+        default: ChallengeStatus.Open
+    }
+}, { timestamps: true })
+
+export default model<IChallenge>('Challenge', challengeSchema)
