d375995b5c203bf4ba0fbd6b49ccef6f29d38e29
John Katua <johnkatua99@gmail.com>
2025-06-22 11:50:42 +0300

    feat: add user badge interface

---
+++ src/interface/user_badge.ts
@@ -0,0 +1,8 @@
+import { Document, Types } from "mongoose";
+
+export interface IUserBadge extends Document {
+    user_id: Types.ObjectId,
+    badge_id: Types.ObjectId,
+    earned_at: Date,
+    is_unlocked: Boolean
+}
