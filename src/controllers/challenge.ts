2821b834b12375e5a1cb152610c8cd2ebbd8b6dd
John Katua <johnkatua99@gmail.com>
2025-06-29 10:15:38 +0300

    feat: add a controller to update challenges

--- src/controllers/challenge.ts
+++ src/controllers/challenge.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { CreateChallengeDto } from "../dto/challenge.dto";
+import { CreateChallengeDto, UpdateChallengeDto } from "../dto/challenge.dto";
 import { ChallengeService } from "../services/challenge";
 
 const challengeService = new ChallengeService()
@@ -10,6 +10,42 @@ export const createChallenge = async (req: Request, res: Response, next: NextFun
         const challenge = await challengeService.create(data);
         res.status(201).json({
             data: challenge
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getChallenges = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const challenges = await challengeService.findAll();
+        res.json({
+            data: challenges
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getChallengeById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const challenge = await challengeService.findById(id)
+        res.json({
+            data: challenge
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateChallenge = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const data = UpdateChallengeDto.parse(req.body)
+        const updatedChallenge = await challengeService.updateById(id, data)
+        res.json({
+            data: updatedChallenge
         })
     } catch (error) {
         next(error)
