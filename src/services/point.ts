ef45a2effb0b7dfe9cdffdf53d294391ace2936d
John Katua <johnkatua99@gmail.com>
2025-06-29 17:10:24 +0300

    add a service method to retrieve all points for a user

--- src/services/point.ts
+++ src/services/point.ts
@@ -1,4 +1,4 @@
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { IPoint } from "../interface/point";
 import { GenericService } from "./generic";
 
@@ -10,5 +10,24 @@ export class PointService extends GenericService<IPoint> {
 
     async findAllByUserId(userId: string) {
         return await this.model.find({ user_id: userId }).sort("-timestamp").exec()
+    }
+
+    async getTotalPointsForUser(userId: string) {
+        const result = await this.model.aggregate([
+            {
+                $match: {
+                    user_id: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    total: {
+                        $sum: "$points"
+                    }
+                }
+            }
+        ])
+        return result.length > 0 ? result[0].total : 0
     }
 }
