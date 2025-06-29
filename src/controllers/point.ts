cfd58e3c62c9ff1a2d612aeab30202fd4c737b1f
John Katua <johnkatua99@gmail.com>
2025-06-29 16:58:57 +0300

    feat: add a controller method to retrieve points for a specific user

--- src/controllers/point.ts
+++ src/controllers/point.ts
@@ -1,5 +1,8 @@
 import Point from "../models/point";
 import { PointService } from "../services/point";
+import { RequestHandlerAsync } from "../types/controller.types";
+import { asyncWrapper } from "../utils/async_wrapper";
+import { successResponse } from "../utils/response.utils";
 import { BaseController } from "./common/base";
 
 const service = new PointService(Point);
@@ -8,4 +11,10 @@ export class PointController extends BaseController<any> {
     constructor() {
         super(service)
     }
+
+    getAllByUserId: RequestHandlerAsync = asyncWrapper(async (req, res) => {
+        const { userId } = req.params;
+        const items = await service.findAllByUserId(userId)
+        return successResponse(res, items)
+    })
 }
