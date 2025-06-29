7845eea5b2181f530b2bfde23d85535dc469d748
John Katua <johnkatua99@gmail.com>
2025-06-29 15:40:52 +0300

    feat: add function used to return successful response within application

--- src/utils/response.utils.ts
+++ src/utils/response.utils.ts
@@ -4,3 +4,10 @@ export const successResponse = <T>(res: any, data: T, status: number = 200) => {
         data
     })
 }
+
+export const successMsgResponse = (res: any, msg: string, status: number = 200) => {
+    return res.status(status).json({
+        success: true,
+        msg
+    })
+}
